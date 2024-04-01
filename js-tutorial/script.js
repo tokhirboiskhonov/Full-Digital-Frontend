e3b679047e838ff6ea1e8268412880179bba04a1
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-02 04:48:34 +0500

    minified js code.

--- js-tutorial/script.js
+++ js-tutorial/script.js
@@ -1,41 +1,26 @@
-//* Tasks
-
 //! 1. Task
-
-let colorOne = "red";
-let colorTwo = "blue";
-let tempColor;
-
-tempColor = colorOne;
-colorOne = colorTwo;
-colorTwo = tempColor;
-
-console.log(colorOne);
-console.log(colorTwo);
-
+let tempColor,
+  colorOne = "red",
+  colorTwo = "blue";
+(tempColor = colorOne),
+  (colorOne = colorTwo),
+  (colorTwo = tempColor),
+  console.log(colorOne),
+  console.log(colorTwo);
 //! 2. Task
-
-let numOne = 7;
-let numTwo = 19;
-let tempNumber;
-
-tempNumber = numOne;
-numOne = numTwo;
-numTwo = tempNumber;
-
-console.log(numOne);
-console.log(numTwo);
-
+let tempNumber,
+  numOne = 7,
+  numTwo = 19;
+(tempNumber = numOne),
+  (numOne = numTwo),
+  (numTwo = tempNumber),
+  console.log(numOne),
+  console.log(numTwo);
 //! 3. Task
-
-var a = 10;
-var b = 20;
-var tempAB;
-
-tempAB = a;
-a = b;
-b = tempAB;
-
-document.write(`a = ${a}
-<br/>
-b = ${b}`);
+var tempAB,
+  a = 10,
+  b = 20;
+(tempAB = a),
+  (a = b),
+  (b = tempAB),
+  document.write(`a = ${a}\n<br/>\nb = ${b}`);
