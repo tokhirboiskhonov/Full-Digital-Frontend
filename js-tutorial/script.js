43807905363782a4e07777ffb6cf0fdbdaac6924
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-04 01:19:13 +0500

    feat: add 4th FizzBuzz task.

--- js-tutorial/script.js
+++ js-tutorial/script.js
@@ -15,3 +15,17 @@ var a = 10,
   b = 20;
 [b, a] = [a, b];
 document.write(`a = ${a}\n<br/>\nb = ${b}`);
+//! 4. Task
+function fizzBuzz(input) {
+  if (input % 3 === 0 && input % 5 === 0) {
+    return "FizzBuzz";
+  } else if (input % 3 === 0) {
+    return "Fizz";
+  } else if (input % 5 === 0) {
+    return "Buzz";
+  } else {
+    return input;
+  }
+}
+const output = fizzBuzz(15);
+console.log(output); // FizzBuzz
