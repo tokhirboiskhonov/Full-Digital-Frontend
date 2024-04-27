4faa47f4710a30436b55dae07afea49d3d0cf881
tokhirboiskhonov <tokhir060@gmail.com>
2024-04-27 09:01:47 +0500

    Logical operators && || !

--- js-tutorial/script.js
+++ js-tutorial/script.js
@@ -4,100 +4,29 @@ const myAge = 17;
 
 const accessTheatre = myAge >= 18 ? "Yes" : "No";
 
-//! 8. Task
+// console.log(accessTheatre);
 
-const marks = [80, 80, 50];
+//* Logical operators && || !
 
-function calculateGrade(marks) {
-  if (marks.length === 0) {
-    return 0;
-  }
+console.log(false && false); // false
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(true && true); // true
 
-  let sum = 0;
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false
+console.log(true || true); // true
+console.log(0 || null || false || NaN || "" || "abc"); // abc
+console.log(0 && null && true && NaN && "" && "abcdefgh"); // 0
 
-  for (let item of marks) {
-    sum += item;
-  }
+console.log(!true); // false
+console.log(!1); // false
+console.log(!0); // true
 
-  const average = sum / marks.length;
+console.log((true && false) || true || (true && false)); // true
+console.log(false || (false && true) || (false && true && false)); // false
 
-  if (average >= 90 && average <= 100) {
-    console.log("A");
-  } else if (average >= 80 && average <= 89) {
-    console.log("B");
-  } else if (average >= 70 && average <= 79) {
-    console.log("C");
-  } else if (average >= 60 && average <= 69) {
-    console.log("D");
-  } else if (average >= 1 && average <= 59) {
-    console.log("F");
-  } else {
-    console.log("Sorry, something went wrong!");
-  }
-}
-
-calculateGrade(marks);
-
-//? 2--version
-
-/* const marks = [80, 80, 50];
-
-function calculateGrade(marks) {
-  if (marks.length === 0) {
-    return 0;
-  }
-
-  let sumInitial = 0;
-
-  let sum = marks.reduce((acc, curVal) => acc + curVal, sumInitial); //! Using reduce to calculate the sum of array elements with an initial sum value.
-
-  const average = sum / marks.length;
-
-  if (average >= 90 && average <= 100) {
-    console.log("A");
-  } else if (average >= 80 && average <= 89) {
-    console.log("B");
-  } else if (average >= 70 && average <= 79) {
-    console.log("C");
-  } else if (average >= 60 && average <= 69) {
-    console.log("D");
-  } else if (average >= 1 && average <= 59) {
-    console.log("F");
-  } else {
-    console.log("Sorry, something went wrong!");
-  }
-}
-
-calculateGrade(marks); */
-
-//! 9. Task
-
-function sum(limit) {
-  let sum = 0;
-  for (let i = 0; i < limit; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      sum += i;
-    }
-  }
-
-  return sum;
-}
-
-console.log(sum(10));
-
-//! 10. Task
-
-function showStarts(rows) {
-  for (let i = 1; i <= rows; i++) {
-    let stars = "";
-    for (let j = 0; j < i; j++) {
-      stars += "*";
-    }
-    console.log(stars);
-  }
-}
-
-showStarts(10);
 
 //! 11. Task
 
