0a7f2d11a31fa2601f1b17705acd6ee5a11adbf5
tokhirboiskhonov <tokhir060@gmail.com>
2024-05-10 11:55:33 +0500

    reafactored some codes convert to comment

--- js-tutorial/script.js
+++ js-tutorial/script.js
@@ -1,4 +1,5 @@
-// Object
+/* 
+* Object
 
 let userData = {
   firstName: "John",
@@ -7,47 +8,51 @@ let userData = {
   isMarried: false,
 };
 
-// console.log(userData);
+console.log(userData);
 
-// 2 xil yol bn qiymatni olishimiz mumkin.
+? 2 xil yol bn qiymatni olishimiz mumkin.
 
-// dot notation (I-usul)
+? dot notation (I-usul)
 
-// console.log(userData.firstName);
+console.log(userData.firstName);
 
-// bracket notation (II-usul)
+? bracket notation (II-usul)
 
-// console.log(userData["age"]);
+console.log(userData["age"]); */
 
-// JS da ma'lumotlarni chiqarish
+/* 
+* JS da ma'lumotlarni chiqarish
 
-// 1. console.log()
-// console.log("Hello World!");
+? 1. console.log()
+console.log("Hello World!");
 
-// 2. document.write()
+? 2. document.write()
 let fName = "Tokhirkhuja";
-// document.write(fName);
+document.write(fName);
 
-// 3. alert()
+? 3. alert()
 
-// alert(fName);
+alert(fName);
 
-// Dasturlash tillarda 2 xil typing languages
+* Dasturlash tillarda 2 xil typing languages
 
-//  1. Static Typing (Java, C#, C++, Scala)
-// 2. Dynamic Typing (JS, Python ...)
+? 1. Static Typing (Java, C#, C++, Scala)
+? 2. Dynamic Typing (JS, Python ...)
+*/
 
-//* Ternary operator
+/* 
+* Ternary operator
 
 const myAge = 17;
 
 const accessTheatre = myAge >= 18 ? "Yes" : "No";
 
-// console.log(accessTheatre);
+console.log(accessTheatre); */
 
-//* Logical operators && || !
+/*
+* Logical operators && || !
 
-/* console.log(false && false); // false
+console.log(false && false); // false
 console.log(true && false); // false
 console.log(false && true); // false
 console.log(true && true); // true
@@ -64,11 +69,13 @@ console.log(!1); // false
 console.log(!0); // true
 
 console.log((true && false) || true || (true && false)); // true
-console.log(false || (false && true) || (false && true && false)); // false */
+console.log(false || (false && true) || (false && true && false)); // false 
+*/
 
-//* Switch Case
+/*
+* Switch Case
 
-/* function weekDay(day) {
+ function weekDay(day) {
   let today = day;
   let result = "";
 
@@ -104,9 +111,9 @@ console.log(false || (false && true) || (false && true && false)); // false */
 
 console.log(weekDay(3)); */
 
-//* Logical operators with Non-boolean
-
-/* Falsy:
+/*
+* Logical operators with Non-boolean
+Falsy:
 		undefined,
 		null,
 		NaN,
@@ -120,20 +127,21 @@ console.log(weekDay(3)); */
 		" "
 */
 
-/* Loop:
+/* 
+	* Loop:
 		For
 		while
-		Do while */
+		Do while 
 
-/* // for loop
+? for loop
 let potatos = 15;
 
 for (let i = 1; i <= potatos; i++) {
   console.log(`${i} ta kartoshka archildi`);
 }
- */
 
-/* // While
+
+? While
 let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 150];
 
 let sum = 0;
@@ -144,9 +152,9 @@ while (i < nums.length) {
   i++;
 }
 
-console.log(sum); */
+console.log(sum); 
 
-/* // Do while
+? Do while
 
 let potatoCount = 0;
 let i = 1;
@@ -154,28 +162,30 @@ let i = 1;
 do {
   console.log(`${i} ta kartoshka archildi`);
   i++;
-} while (i <= potatoCount); */
+} while (i <= potatoCount); 
+*/
 
-// Infinitive Loops
-// For in, its only for object
-// For of
+/* 
+* Infinitive Loops
+* For in, its only for object
+* For of
 
-/* let data = {
+ let data = {
   firstName: "John",
   lastName: "Doe",
   age: 20,
   isMarried: false,
 };
 
-// for (key in data) {
-//   console.log(`${key}: ${data[key]}`);
-// }
+ for (key in data) {
+   console.log(`${key}: ${data[key]}`);
+ }
 
 const keys = Object.keys(data);
-// console.log(keys);
+console.log(keys);
 for (let i = 0; i < keys.length; i++) {
   console.log(keys[i]);
-} */
+}
 
 let users = [
   {
@@ -191,7 +201,7 @@ let users = [
 ];
 
 for (let user of users) {
-  //   console.log(user);
+	console.log(user);
   for (let key in user) {
     console.log(`${key}: ${user[key]}`);
   }
@@ -201,10 +211,10 @@ let movies = ["Harry", "Your step", "Batman", "Spiderman"];
 
 for (let item of movies) {
   console.log(item);
-}
+} */
 
-//* Objects(Obyektlar)
-// #1 -> Basic Objects (Obyektlar asosi)
+/* * Objects(Obyektlar)
+? #1 -> Basic Objects (Obyektlar asosi)
 
 let obj = {
   firstName: "Tokhirkhuja",
@@ -215,7 +225,7 @@ let obj = {
 
 console.log(obj);
 
-// #2 -> Object properties and methods
+? #2 -> Object properties and methods
 
 const obj2 = {
   city: "Fergana",
@@ -228,23 +238,23 @@ const obj2 = {
   },
 };
 
-// obj2.func1();
-// obj2.func2();
+obj2.func1();
+obj2.func2();
 
-//* Objectga qo'shish, dynamic add
+* Objectga qo'shish, dynamic add
 
 obj2.name = "John Smith";
 
-// console.log(obj2);
+console.log(obj2);
 
-//* Objectdan o'chirib tashlash, dynamic delete
+* Objectdan o'chirib tashlash, dynamic delete
 
 delete obj2.func1;
 delete obj2.func2;
 
 console.log(obj2);
 
-// #3 -> Objectni ichida bormi yoki yo'qmi
+#3 -> Objectni ichida bormi yoki yo'qmi
 
 const box = {
   calledName: "Square",
@@ -256,9 +266,9 @@ console.log("color" in box); // true
 console.log("number" in box); // true
 console.log("age" in box); // false
 
-// console.log(box);
+console.log(box);
 
-//! Modern ES6 JS features in object
+! Modern ES6 JS features in object
 
 let firstName = "Malikaxon";
 
@@ -269,35 +279,40 @@ let person = {
 
 console.log(person);
 
-/* //* Objects basic (intermediate level)
- JavaScript Object Properties and methods */
+ 
+* Objects basic (intermediate level)
+JavaScript Object Properties and methods
+
 const user = {
   firstName: "John",
   lastName: "Doe",
   age: 25,
 };
 
-//* Object keys, values, entries
+* Object keys, values, entries
 
-/* for (let keys in user) {
+for (let keys in user) {
     console.log(keys, user[keys]);
-} */
+}
 
 const keys = Object.keys(user);
 const values = Object.values(user);
 const entries = Object.entries(user);
+
 console.log(keys);
+
 console.log(values);
+
 console.log(entries);
 
-// for (let item of Object.values(user)) {
-//   console.log(item);
-// }
+for (let item of Object.values(user)) {
+  console.log(item);
+}
 
-//* Clone objects in JS
-// Javascriptdagi obyektlarni klonlash
+* Clone objects in JS
+Javascriptdagi obyektlarni klonlash
 
-// Primitive
+? Primitive
 
 let ism1 = "Shabnam";
 let ism2 = ism1;
@@ -307,18 +322,18 @@ ism2 = "Zuhrakhon";
 console.log(ism1);
 console.log(ism2);
 
-// Non-primitive (reference data types)
+? Non-primitive (reference data types)
 let user1 = {
   name: "Ali",
   age: 24,
 };
 
-// let user2 = user1;
+let user2 = user1;
 
-// user2.name = "Vali";
+user2.name = "Vali";
 
-// console.log(user2);
-// console.log(user1);
+console.log(user2);
+console.log(user1);
 
 let clone = {};
 
@@ -329,23 +344,23 @@ for (let key in user1) {
 clone.name = "Jack";
 clone.age = 30;
 
-// console.log(clone); // { name: 'Jack', age: 30 }
-// console.log(user1); // { name: 'Ali', age: 24 }
+console.log(clone); // { name: 'Jack', age: 30 }
+console.log(user1); // { name: 'Ali', age: 24 }
 
-//! 1. Object.assign
+! 1. Object.assign
 
 let user2 = Object.assign({}, user1);
 user2.name = "John";
 user2.age = 42;
 console.log(user2); // { name: 'John', age: 42 }
 
-//! 2. Spread Operator
+! 2. Spread Operator
 
 let user3 = { ...user1 };
 user3.name = "Azizbek";
 console.log(user3);
 
-//! Copy with nested obj
+! Copy with nested obj
 
 let person2 = {
   name: "Angelina",
@@ -361,11 +376,12 @@ exPerson.address = { ...exPerson.address };
 exPerson.address.state = "KLCC";
 exPerson.address.city = "Langkawi";
 console.log(exPerson);
-console.log(person2);
-
-//* String(methods)
+console.log(person2); */
 
 /*
+* String(methods)
+
+
 - charAt
 - startWith || endWith
 - includes
@@ -373,16 +389,16 @@ console.log(person2);
 - toUpperCase || toLowerCase
 - subString
 - replace
-*/
 
-//? charAt - nechinchi indexdagi charni aniqlab beradi.
+
+? charAt - nechinchi indexdagi charni aniqlab beradi.
 
 let word = "Hello";
 let res = word.charAt(4); // o
 console.log(res);
 console.log(word[1]); // e
 
-//? startWith - boshlanadimi?
+? startWith - boshlanadimi?
 
 let text = "Welcome to JavaScript world.";
 
@@ -390,26 +406,26 @@ console.log(text.startsWith("Welcome")); // true
 console.log(text.startsWith("W")); // true
 console.log(text.startsWith("el")); // false
 
-//? endsWith - boshlanadimi?
+? endsWith - boshlanadimi?
 
 console.log(text.endsWith("or")); // false
 console.log(text.endsWith("ld")); // false
 
-//? includes
+? includes
 
 let message = "You are selected to our university";
 
 console.log(message.includes("are")); //true
 console.log(message.includes("  to")); // false, because of two empty space
 
-//? indexOf || lastIndexOf
+? indexOf || lastIndexOf
 
 let mes = "You are registered to membership";
 
 console.log(mes.indexOf("i")); // 11
 console.log(mes.lastIndexOf("i")); // 30
 
-//? toUpperCase || toLowerCase
+? toUpperCase || toLowerCase
 
 let test = "hello guys, how are you doing?";
 let upperTest = test.toUpperCase();
@@ -417,14 +433,14 @@ let lowerTest = test.toLowerCase();
 console.log(upperTest); // HELLO GUYS, HOW ARE YOU DOING?
 console.log(lowerTest); // hello guys, how are you doing?
 
-// practise
+! practise
 
 const text1 = "Lorem ipsum dolor sit";
 const searchOutput = "IPSum";
 const findWords = text1.toLowerCase().includes(searchOutput.toLowerCase());
 console.log(findWords);
 
-//? subString - ayrim ma'lum bir qismini qirqib olish uchun.
+? subString - ayrim ma'lum bir qismini qirqib olish uchun.
 
 const test1 = "Frontend Developer";
 
@@ -433,49 +449,52 @@ const resultTest2 = test1.substring(9, 18);
 console.log(resultTest1);
 console.log(resultTest2);
 
-//? replace - alishtirib qo'yish
+? replace - alishtirib qo'yish
 
 let test2 = "You are our member and you are registered.";
 
 let resultTest3 = test2.replace("member", "subscriber");
 console.log(resultTest3);
 
-//* Template literals (ES6 xususiyati - interpolatsiyalash) ``
+* Template literals (ES6 xususiyati - interpolatsiyalash) ``
 
 let Tname = "Tokhirkhuja",
   Tsurname = "Boiskhonov",
   age = 24;
 
 console.log(`My full name is ${Tname} ${Tsurname} and I'm ${age} years old!`);
+*/
 
-//* Date
+/* 
+* Date
 
 const today = new Date();
-// console.log(today);
+console.log(today);
 
 let todayDate = today.getDate();
-// console.log(todayDate);
+console.log(todayDate);
 
 let todayMonth = today.getMonth();
-// console.log(todayMonth);
+console.log(todayMonth);
 
 let todayHours = today.getHours();
-// console.log(todayHours);
+console.log(todayHours);
 
 let todayMinutes = today.getMinutes();
-// console.log(todayMinutes);
+console.log(todayMinutes);
 
 let todayYear = today.getFullYear();
-// console.log(todayYear);
+console.log(todayYear);
 
 const timeInMs = Date.now();
-// console.log(timeInMs);
+console.log(timeInMs);
 console.log(timeInMs / 1000 / 60 / 60 / 24 / 365);
-
-//* Arrays
+*/
 
 /*
-#1 -> Adding and Removing elements
+* Arrays
+
+! #1 -> Adding and Removing elements
 
 Oxiridan--
 	-push
@@ -485,32 +504,28 @@ Boshidan--
 	-shift
 Xohlagan joydan--
 	-splice
-*/
+
 
 let fruits = ["apple", "pear", "melon"];
-// console.log(fruits);
+console.log(fruits);
 
-//? push
-// fruits.push("waterMelon");
-// console.log(fruits); // [ 'apple', 'pear', 'melon', 'waterMelon' ]
+? push
+fruits.push("waterMelon");
+console.log(fruits); // [ 'apple', 'pear', 'melon', 'waterMelon' ]
 
-//? pop
+? pop
+fruits.pop();
+console.log(fruits); // [ 'apple', 'pear' ]
 
-// fruits.pop();
-// console.log(fruits); // [ 'apple', 'pear' ]
+? unshift
+fruits.unshift("cherry");
+console.log(fruits); // [ 'cherry', 'apple', 'pear', 'melon' ]
 
-//? unshift
-
-// fruits.unshift("cherry");
-// console.log(fruits); // [ 'cherry', 'apple', 'pear', 'melon' ]
-
-//? shift
-
+? shift
 fruits.shift();
 console.log(fruits); // ['pear', 'melon']
 
-//? splice
-
+? splice
 const numbers = [1, 2, 3, 4, 5];
 
 // numbers.splice(1, 2, "Hello");
@@ -519,13 +534,11 @@ console.log(numbers); // [1, 2, 'book', 'pen', 'sweet', 5]
 
 //* Finding elements -> (Primitive-types)
 
-/*
 1) indexOf
 2) lastIndexOf
 3) includes 
-*/
 
-//? indexOf
+? indexOf
 
 const cars = ["malibu", "spark", true, 7, "spark"];
 
@@ -533,17 +546,17 @@ console.log(cars.indexOf("spark")); // 1
 console.log(cars.indexOf(true)); // 2
 console.log(cars.indexOf("malibu")); // 0
 
-//? lastIndexOf
+? lastIndexOf
 
 console.log(cars.lastIndexOf("spark")); // 4
 
-//? includes (tekshiradi?)
+? includes (tekshiradi?)
 
 console.log(cars.includes("spark")); // true
 console.log(cars.includes("captiva")); // false
 console.log(cars.includes("ma")); // false
 
-//? concat()
+? concat()
 
 let num1 = [1, 2, 3];
 let num2 = [4, 5, 6];
@@ -551,16 +564,16 @@ let num2 = [4, 5, 6];
 let num3 = num1.concat(num2);
 console.log(num3);
 
-//? slice
+? slice
 
 let movies1 = ["Forsache", "Vanishing Time", "Steve", "Novda"];
 
 let newMovies1 = movies1.slice(1, 4);
 console.log(newMovies1); // [ 'Vanishing Time', 'Steve', 'Novda' ], it returns new copied array
 
-//? Spread operator
+? Spread operator
 
-/* let films = ["Harry", "Steve", "Novda"];
+let films = ["Harry", "Steve", "Novda"];
 let newHero = films;
 
 newHero[0] = "Aziz";
@@ -571,7 +584,8 @@ let newMovies2 = [...films];
 console.log(films);
 console.log(newMovies2);
 
- console.log(films == newMovies); */
+ console.log(films == newMovies);
+
 
 let myUser = {
   firstName: "Tokhirkhuja",
@@ -586,31 +600,33 @@ myUser1.city = "Tashkent";
 console.log(myUser);
 console.log(myUser1);
 
-//* Iterating an Array (for of, for in)
+*/
+/* 
+* Iterating an Array (for of, for in)
 
 const arr = [1, 2, 3, 4, 5];
 
-//? for of
+? for of
 
-/* let sum = 0;
+let sum = 0;
 for (const val of arr) {
   sum += val;
 }
-console.log(sum); */
+console.log(sum);
 
-//? for in
+? for in
 
 for (const index in arr) console.log(index); // 0 1 2 3 4
 
-//* join and split
+* join and split
 
-//? join -> join methods help convert from array to string
+? join -> join methods help convert from array to string
 
 const cardNumbers = [7, 9, 15, 6];
 const cardNumbersJoined = cardNumbers.join("");
 console.log(cardNumbersJoined);
 
-//? split -> split methods help convert from string to array
+? split -> split methods help convert from string to array
 
 let animals = "dog-cat-chicken-pig-wolf";
 
@@ -626,11 +642,11 @@ let text2 = "Telegram";
 
 let letterText = text2.split("");
 
-console.log(letterText); // ['T', 'e', 'l', 'e', 'g', 'r', 'a', 'm']
+console.log(letterText); // ['T', 'e', 'l', 'e', 'g', 'r', 'a', 'm'] */
+/* 
+! Guess number game
 
-//? Guess number game
-
-/* function guessNumber() {
+function guessNumber() {
   let randomNum = Math.floor(Math.random() * 11);
 
   let guess;
