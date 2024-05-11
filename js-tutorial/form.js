e46c96f6b6238b3d4ce16df5f5f49524f4892fd5
tokhirboiskhonov <tokhir060@gmail.com>
2024-05-12 01:19:16 +0500

    feat: add some js code with event listener

---
+++ js-tutorial/form.js
@@ -0,0 +1,39 @@
+const inputName = document.getElementById("name");
+const inputAge = document.getElementById("age");
+const inputMarried = document.getElementById("married");
+const elName = document.querySelector(".name");
+const elAge = document.querySelector(".age");
+const elMarried = document.querySelector(".married");
+const submitBtn = document.querySelector(".btn-submit");
+const cancelBtn = document.querySelector(".btn-cancel");
+const resultBox = document.querySelector(".result__box");
+
+inputName.addEventListener("keyup", () => {
+  let val = inputName.value;
+  elName.textContent = `💭 Name: ${val}`;
+});
+
+inputAge.addEventListener("keyup", () => {
+  let val = inputAge.value;
+  elAge.textContent = `🧐 Age: ${val}`;
+});
+
+inputMarried.addEventListener("keyup", () => {
+  let val = inputMarried.value;
+  elMarried.textContent = `😘 Is married: ${val}`;
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  resultBox.innerHTML = "Successful send! ✅";
+});
+
+cancelBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  inputName.value = "";
+  inputAge.value = "";
+  inputMarried.value = "";
+  elName.textContent = "";
+  elAge.textContent = "";
+  elMarried.textContent = "";
+});
