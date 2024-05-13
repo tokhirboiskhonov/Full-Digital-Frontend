b93a069d446f266af602c09ec3749a5d3bd2d73f
tokhirboiskhonov <tokhir060@gmail.com>
2024-05-14 01:49:57 +0500

    feat: add event listener in js

--- easy-modal/script.js
+++ easy-modal/script.js
@@ -5,3 +5,10 @@ const openModalBtn2 = document.querySelector("#openModalBtn-2");
 const openModalBtn3 = document.querySelector("#openModalBtn-3");
 const closeModalBtn = document.getElementById("closeModalBtn");
 
+// Event Listener
+openModalBtn1.addEventListener("click", openModal);
+openModalBtn2.addEventListener("click", openModal);
+openModalBtn3.addEventListener("click", openModal);
+modalContainer.addEventListener("click", closeModal);
+closeModalBtn.addEventListener("click", closeModal);
+
