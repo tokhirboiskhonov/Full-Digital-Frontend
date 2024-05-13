3e7840074edb4448dbc5b1b3ba95b4c6fa114be7
tokhirboiskhonov <tokhir060@gmail.com>
2024-05-14 01:49:33 +0500

    feat: add selectors tags from html in js

---
+++ easy-modal/script.js
@@ -0,0 +1,7 @@
+// Selectors
+const modalContainer = document.getElementById("modalContainer");
+const openModalBtn1 = document.querySelector("#openModalBtn-1");
+const openModalBtn2 = document.querySelector("#openModalBtn-2");
+const openModalBtn3 = document.querySelector("#openModalBtn-3");
+const closeModalBtn = document.getElementById("closeModalBtn");
+
